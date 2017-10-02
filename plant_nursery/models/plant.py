d10daef257dfa08b789c1058c8fcd4428cae430d
Thibault Delavallée <tde@odoo.com>
2017-10-02 17:23:57 +0200

    [ADD] plant: add website.seo.metadata on plant

--- plant_nursery/models/plant.py
+++ plant_nursery/models/plant.py
@@ -25,7 +25,7 @@ class Plants(models.Model):
     _name = 'plant.plant'
     _description = 'Plant'
     _order = 'name'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _inherit = ['mail.thread', 'mail.activity.mixin', 'website.seo.metadata']
 
     name = fields.Char('Plant Name', required=True)
     # description
